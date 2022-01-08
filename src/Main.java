f084a78e342bf68b4a90129abec1326392cdae45
Kotyara <babanina.ev@gmail.com>
2022-01-08 21:53:34 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public int addOne(int count) {
+    return count++;
+  }
+
+  public void hello() {
+    System.out.println("Hello!");
+  }
+
+  private String cat (int count) {
+    return "Count of cats: " + count;
+  }
+
+}
